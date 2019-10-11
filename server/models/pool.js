7edc494feadcf229618488102614b6a1d066ec4c
Christophe <powerforweb@gmail.com>
2019-10-11 14:02:04 +0200

    maj gitlab models

--- server/models/pool.js
+++ server/models/pool.js
@@ -4,12 +4,14 @@ const mongoose = require('mongoose');
 
 let PoolSchema = new mongoose.Schema({
     index: { type: String },
+    idx: { type: String },
+    id: { type: String },
     id_cmd: { type: String },
     onetime: { type: Number },
     subscription: { type: Number },
     eid: { type: Number },
     contractID: { type: String },
-    qhid: { type: Number },
+    qhid: { type: String },
     quotation_level: { type: String },
     begin_date: { type: String },
     end_date: { type: String },
