00e4e541f58723798d4b5e819d6aa98d44fcf036
Sadok Jbenyeni <sadok.jbenyeni@iress.com>
2020-03-23 16:59:00 +0100

    Working on tests

--- site/src/app/components/admin/user-detail/user-detail.component.spec.ts
+++ site/src/app/components/admin/user-detail/user-detail.component.spec.ts
@@ -1,5 +1,6 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { async, ComponentFixture, TestBed, inject } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
+import { HttpClientModule } from '@angular/common/http';
 import { UserDetailComponent } from './user-detail.component';
 
 describe('UserDetailComponent', () => {
@@ -8,6 +9,7 @@ describe('UserDetailComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
+      imports: [HttpClientModule],
       declarations: [ UserDetailComponent ]
     })
     .compileComponents();
--- site/src/app/components/product/orders-view/ordersp-view.component.spec.ts
+++ site/src/app/components/product/orders-view/ordersp-view.component.spec.ts
@@ -1,5 +1,5 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { ActivatedRoute } from '@angular/router'
 import { OrderspViewComponent } from './ordersp-view.component';
 
 describe('OrderspViewComponent', () => {
@@ -8,6 +8,7 @@ describe('OrderspViewComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
+      imports : [ActivatedRoute],
       declarations: [ OrderspViewComponent ]
     })
     .compileComponents();
