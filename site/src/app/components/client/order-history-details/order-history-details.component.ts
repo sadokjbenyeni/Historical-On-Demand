25fa4bf04126736425d290e71c5fb7f2b4f561f8
Sadok Jbenyeni <sadok.jbenyeni@iress.com>
2020-05-14 12:26:23 +0100

    Premerging with Yassine

--- site/src/app/components/client/order-history-details/order-history-details.component.ts
+++ site/src/app/components/client/order-history-details/order-history-details.component.ts
@@ -273,10 +273,10 @@ export class OrderHistoryDetailsComponent implements OnInit {
         });
         let newProduct = new Data(index, product.dataset, product.qhid, product.eid, product.symbol, product.description, product.assetClass, product.exchangeName, product.mics, null, product.period, product.begin_date_select, product.end_date_select, product.pricingTier, product.ht, product.links, product.links, product);
         this.details.push(newProduct);
-        if (product.backfill_fee > 0 || product.ongoing_fee > 0) {
-          this.print = true;
-          this.details.push({ backfill_fee: product.backfill_fee, ongoing_fee: product.ongoing_fee });
-        }
+        // if (product.backfill_fee > 0 || product.ongoing_fee > 0) {
+        //   this.print = true;
+        //   this.details.push({ backfill_fee: product.backfill_fee, ongoing_fee: product.ongoing_fee });
+        // }
       });
     }
     this.dataSource.data = this.details;
