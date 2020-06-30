095cc946537a5cc4f3a15ff84e60c71986548020
Sadok Jbenyeni <sadok.jbenyeni@iress.com>
2020-06-30 15:15:37 +0100

    Merging bug: Fixing merge bugs with Yassine

--- server/api/v1/invoice.js
+++ server/api/v1/invoice.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const InvoiceService = require('../../service/invoiceService');
 const Orders = mongoose.model('Order');
 const Invoices = mongoose.model('Invoice');
-path = require('path');
+const path = require('path');
 var mime = require('mime');
 const OrderPdfService = require('../../service/orderPdfService');
 
