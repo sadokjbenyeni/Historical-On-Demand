b01164409fea91539466b998f09a061b6b60193f
Sadok Jbenyeni <sadok.jbenyeni@iress.com>
2020-07-23 10:48:12 +0100

    Worked on invoice model desing and other improvements

--- Backend/Historical_On_Demand.Server/server/service/orderPdfService.js
+++ Backend/Historical_On_Demand.Server/server/service/orderPdfService.js
@@ -19,11 +19,11 @@ module.exports = function (order) {
     var user = await User.findOne({ _id: this.order.userId }).exec();
     var country = await Country.findOne({ id: this.order.countryBilling }).exec();
     var currency = await Currency.findOne({ id: this.order.currency }).exec();
-    this.generatePdfFile(logger, currency, user, country, invoiceId, invoiceType);
+    this.generatePdfFile(currency, user, country, invoiceId, invoiceType);
     return true;
   }
 
-  this.generatePdfFile = function (logger, currency, user, country, invoiceId, invoiceType) {
+  this.generatePdfFile = function (currency, user, country, invoiceId, invoiceType) {
     let fonts = {
       GentiumBookBasic: {
         normal: new Buffer(require('pdfmake/build/vfs_fonts.js').pdfMake.vfs['gentium-book-basic.regular.ttf'], 'base64'),
@@ -32,81 +32,38 @@ module.exports = function (order) {
         bolditalics: new Buffer(require('pdfmake/build/vfs_fonts.js').pdfMake.vfs['gentium-book-basic.bold-italic.ttf'], 'base64')
       }
     };
+
     printer = new pdfMake(fonts);
-    style = {
-      // Document Header
-      datacmd: { alignment: 'right' },
-      documentHeaderLeft: { fontSize: 10, margin: [0, 0, 0, 0], alignment: 'left' },
-      documentHeaderCenter: { fontSize: 10, margin: [0, 0, 0, 0], alignment: 'center' },
-      documentHeaderRight: { fontSize: 10, margin: [0, 0, 0, 0], alignment: 'right' },
-      // Document Footer
-      documentFooterLeft: { fontSize: 10, margin: [0, 0, 0, 0], alignment: 'left' },
-      documentFooterCenter: { fontSize: 10, margin: [0, 0, 0, 0], alignment: 'center' },
-      documentFooterRight: { fontSize: 10, margin: [0, 0, 0, 0], alignment: 'right' },
-      // Invoice Title
-      invoiceTitle: { fontSize: 22, bold: true, alignment: 'right', margin: [0, 0, 0, 15] },
-      // Invoice Details
-      invoiceSubTitle: { fontSize: 10, alignment: 'left' },
-      invoiceSubValue: { fontSize: 10, alignment: 'right' },
-      // Billing Headers
-      invoiceBillingTitle: { fontSize: 14, bold: true, alignment: 'left', margin: [0, 20, 0, 5], },
-      // Billing Details
-      invoiceBillingDetails: { alignment: 'left' },
-      invoiceBillingAddressTitle: { fontSize: 10, margin: [5, 5, 5, 5] },
-      invoiceBillingAddress: { fontSize: 8, margin: [5, 5, 5, 5] },
-      // Items Header
-      itemsHeader: { margin: [0, 5, 0, 5], bold: true },
-      // Item Title
-      itemTitle: { bold: true, },
-      // itemSubTitle: { italics: true, fontSize: 8},
-      itemSubTitle: { fontSize: 8, },
-      itemNumber: { margin: [0, 5, 0, 5], alignment: 'center', },
-      itemTotal: { margin: [0, 5, 0, 5], bold: true, alignment: 'center', },
-
-      // Items Footer (Subtotal, Total, Tax, etc)
-      itemsFooterSubTitle: { margin: [0, 5, 0, 5], bold: true, alignment: 'right', fontSize: 10 },
-      itemsFooterSubValue: { margin: [0, 5, 0, 5], bold: true, alignment: 'center', fontSize: 10 },
-      itemsFooterTotalTitle: { margin: [0, 5, 0, 5], bold: true, alignment: 'right', fontSize: 10 },
-      itemsFooterTotalValue: { margin: [0, 5, 0, 5], bold: true, alignment: 'center', fontSize: 10 },
-      signaturePlaceholder: { margin: [0, 70, 0, 0], },
-      signatureName: { bold: true, alignment: 'center', },
-      signatureJobTitle: { italics: true, fontSize: 10, alignment: 'center', },
-      notesTitle: { fontSize: 10, bold: true, margin: [0, 50, 0, 3] },
-      notesText: { fontSize: 10 },
-      center: { alignment: 'center' },
-    };
-
     let invoice = {};
-    let content = [];
     let defaultStyle = {
       font: 'GentiumBookBasic'
     };
+
     var invoiceInformation = { id: invoiceId, type: invoiceType, issueDate: toCalendarFormat(new Date()), dueDate: toCalendarFormat(this.order.submissionDate) };
     var client = { company: this.order.companyName, address: this.order.addressBilling, postalCode: this.order.postalCodeBilling, city: this.order.cityBilling, country: this.order.countryBilling };
     var company = { name: 'QUANTHOUSE', address: '86 boulevard Haussmann', postalCode: '75008', city: 'Paris', country: 'France' };
     var payment = { accountNumber: user.id, taxId: this.order.vat, orderId: this.order.orderId, currency: currency.name };
+
+    let content = [];
     content.push(
       headerWithLogo(invoiceInformation, client, company, payment),
-      orderTableHeader(),
+      orderListHeader(),
       drawLine(0, 555, 0, 0, 3, 'line', '#EA663B'),
-      getOrders(this.order.products, this.order.vatValue, country),
+      getOrders(this.order.products),
       marginBottom(10),
       amountTable(this.order.totalHTDiscountFree, this.order.totalVat, this.order.vatValue, this.order.total, this.order.discount, currency, this.order.totalHT),
       drawLine(342, 560, -3, -3, 1, 'line', '#EA663B'),
       generalPaymentTerms(),
       drawLine(340, 560, 0, 0, 0.01, 'line')
-
     );
+
     let paginator = {};
     paginator = function (currentPage, totalPage) {
       return [{ text: currentPage.toString() + ' of ' + totalPage, alignment: 'center', fontSize: 8 }]
     };
+
     let footer = []
-    let sasu = 'SASU au capital de 14.108.818 €';
-    let rcs = 'RCS Paris 449703248';
-    let vat = 'VAT : FR00449703248';
-    footer = bottom(currency, country, this.order.vatValide, sasu, rcs);
-    invoice['style'] = style;
+    footer = bottom(currency, country, this.order.vatValide);
     invoice['pageMargins'] = [15, 15, 15, 105];
     invoice['defaultStyle'] = defaultStyle;
     invoice['page'] = paginator;
@@ -125,61 +82,7 @@ module.exports = function (order) {
   };
 }
 
-condition = function (conditions) {
-  let tabCondition = [];
-  tabCondition.push([{ text: 'General payment terms', bold: true, fontSize: 9, color: '#303030', fillColor: '#F6F6F6' }]);
-  conditions.forEach(cond => {
-    tabCondition.push([{ text: '- ' + cond, fontSize: 9, color: '#6C6C6C', fillColor: '#F6F6F6' }]);
-  });
-  return tabCondition;
-};
-
-wireTransfer = function (currency, sasu, rcs, country, vatok) {
-  const beneficiaryName = 'Quanthouse';
-  const beneficiaryAddress = '86 boulevard Haussmann - 75008 PARIS - FRANCE';
-  const vat = 'FR00449703248';
-  let bic = 'BIC Code :';
-  let bank = 'Receiving Bank Name :';
-  let iban = 'IBAN :';
-
-  if (currency.id == 'usd' || currency.id == 'gbp') {
-    bic = '';
-    bank = '';
-    iban = '';
-  }
-  return {
-    widths: ['30%', '10%', '25%', '10%', '25%'],
-    table: {
-      body: [[
-        [
-          [{ text: [{ text: 'Beneficiary Name : ', fontSize: 9, bold: true, color: '#303030' }, { text: beneficiaryName, fontSize: 9, color: '#6C6C6C' }] }],
-          [{ text: [{ text: 'Beneficiary Address : ', fontSize: 9, bold: true, color: '#303030' }, { text: beneficiaryAddress, fontSize: 9, color: '#6C6C6C' }] }],
-          [{ text: [{ text: 'VAT : ', fontSize: 9, bold: true, color: '#303030' }, { text: vat, fontSize: 9, color: '#6C6C6C' }] }]
-        ],
-        [
-          { text: '' }
-        ],
-        [
-          [{ text: sasu, fontSize: 9, color: '#6C6C6C' }],
-          [{ text: rcs, fontSize: 9, color: '#6C6C6C' }],
-          [{ text: vatType(country, vatok), fontSize: 9, color: '#6C6C6C' }]
-        ],
-        [
-          { text: '' }
-        ],
-        [
-          [{ text: [{ text: bic, fontSize: 9, bold: true, color: '#303030' }, { text: currency.bic, fontSize: 9, color: '#6C6C6C' }] }],
-          [{ text: [{ text: bank, fontSize: 9, bold: true, color: '#303030' }, { text: currency.rib.domiciliation, fontSize: 9, color: '#6C6C6C' }] }],
-          [{ text: [{ text: iban, fontSize: 9, bold: true, color: '#303030' }, { text: currency.iban.ib1 + ' ' + currency.iban.ib2 + ' ' + currency.iban.ib3 + ' ' + currency.iban.ib4 + ' ' + currency.iban.ib5 + ' ' + currency.iban.ib6 + ' ' + currency.iban.ib7, fontSize: 9, color: '#6C6C6C' }] }]
-        ]
-      ]
-      ]
-    },
-    layout: { defaultBorder: false }
-  };
-};
-
-orderTableHeader = function () {
+orderListHeader = function () {
   let border = [false, false, false, true];
   return {
     table: {
@@ -200,8 +103,7 @@ orderTableHeader = function () {
   }
 }
 
-function getOrders(orders, vatValue, country) {
-  var pervat = country.ue === "1" ? vatValue : 0;
+function getOrders(orders) {
   let listOrders = [];
   let border = [false, false, false, true];
   orders.forEach(
@@ -212,7 +114,7 @@ function getOrders(orders, vatValue, country) {
   if (orders.length > 0) {
     orders.forEach(eid => {
       eid.allproducts.forEach(product => {
-        addProduct(listOrders, product, eid.exchangefee, border, pervat, colorCounter);
+        addProduct(listOrders, product, eid.exchangefee, border, colorCounter);
         colorCounter++;
       });
     })
@@ -228,8 +130,7 @@ function getOrders(orders, vatValue, country) {
   }
 };
 
-function addProduct(listOrders, product, exchangefee, border, pervat, colorCounter) {
-  let description = product.description ? product.description : product.contractid;
+function addProduct(listOrders, product, exchangefee, border, colorCounter) {
   var typeOrder = product.onetime === 1 ? "One-Off" : "Subscription";
   var dateDebut = product.onetime === 1 ? toCalendarFormat(product.begin_date) : toCalendarFormat(product.begin_date_ref);
   var dateFin = product.onetime === 1 ? toCalendarFormat(product.end_date) : toCalendarFormat(product.end_date_ref);
@@ -254,21 +155,15 @@ function addProduct(listOrders, product, exchangefee, border, pervat, colorCount
 
 }
 
-insertLineCarriage = function (sentence, insertPostion) {
-  if (insertPostion > 10) return sentence.slice(0, insertPostion) + '\n' + sentence.slice(insertPostion);
-  else return sentence;
-}
 toCalendarFormat = function (date) {
   return new DateService().calenderFormat(date);
 }
-
 
 logo = function () {
   let logo = new LogoService().qhLogo();
   return {
     image: logo, width: 150, height: 50
   }
-
 }
 
 companyAddress = function (company, address, postalCode, city, country) {
@@ -303,7 +198,6 @@ clientAddress = function (companyName, address, postalCode, city, country) {
         [
           [
             { text: 'Client', fontSize: 11, bold: true, color: '#303030' },
-
             { text: '\n', fontSize: 5 },
             { text: companyName, fontSize: 10, bold: true, color: '#303030' },
             { text: address, fontSize: 10, color: '#6C6C6C' },
@@ -463,34 +357,34 @@ amountTable = function (serviceTotal, vatTotal, vatValue, invoiceTotal, discount
               widths: ['60%', '30%', '10%'],
               body: [
                 [
-                  { border: border, text: 'SUBTOTAL', fontSize: 10, style: 'itemsFooterSubTitle', color: '#6C6C6C' },
-                  { border: border, text: currencySymbol + serviceTotal.toFixed(2), fontSize: 10, style: 'itemsFooterSubValue', alignment: 'right', color: '#6C6C6C' },
-                  { border: border, text: currency.device, fontSize: 10, style: 'itemsFooterSubValue', color: '#6C6C6C' }
+                  { border: border, text: 'SUBTOTAL', fontSize: 10, color: '#6C6C6C' },
+                  { border: border, text: currencySymbol + serviceTotal.toFixed(2), fontSize: 10, alignment: 'right', color: '#6C6C6C' },
+                  { border: border, text: currency.device, fontSize: 10, color: '#6C6C6C' }
                 ],
                 [
-                  { border: border, text: 'DISCOUNT', fontSize: 10, style: 'itemsFooterSubTitle', color: '#6C6C6C' },
-                  { border: border, text: discount, fontSize: 10, style: 'itemsFooterSubValue', alignment: 'right', color: '#6C6C6C' },
-                  { border: border, text: '%', fontSize: 10, style: 'itemsFooterSubValue', color: '#6C6C6C' }
+                  { border: border, text: 'DISCOUNT', fontSize: 10, color: '#6C6C6C' },
+                  { border: border, text: discount, fontSize: 10, alignment: 'right', color: '#6C6C6C' },
+                  { border: border, text: '%', fontSize: 10, color: '#6C6C6C' }
                 ],
                 [
-                  { border: border, text: 'SUBTOTAL LESS DISCOUNT', fontSize: 10, style: 'itemsFooterSubTitle', color: '#6C6C6C' },
-                  { border: border, text: currencySymbol + totalLessDiscount, fontSize: 10, style: 'itemsFooterSubValue', alignment: 'right', color: '#6C6C6C' },
-                  { border: border, text: currency.device, fontSize: 10, style: 'itemsFooterSubValue', color: '#6C6C6C' }
+                  { border: border, text: 'SUBTOTAL LESS DISCOUNT', fontSize: 10, color: '#6C6C6C' },
+                  { border: border, text: currencySymbol + totalLessDiscount.toFixed(2), fontSize: 10, alignment: 'right', color: '#6C6C6C' },
+                  { border: border, text: currency.device, fontSize: 10, color: '#6C6C6C' }
                 ],
                 [
-                  { border: border, text: 'TAX RATE', fontSize: 10, style: 'itemsFooterSubTitle', color: '#6C6C6C' },
-                  { border: border, text: vatValue * 100, fontSize: 10, style: 'itemsFooterSubValue', alignment: 'right', color: '#6C6C6C' },
-                  { border: border, text: '%', fontSize: 10, style: 'itemsFooterSubValue', color: '#6C6C6C' },
+                  { border: border, text: 'TAX RATE', fontSize: 10, color: '#6C6C6C' },
+                  { border: border, text: vatValue * 100, fontSize: 10, alignment: 'right', color: '#6C6C6C' },
+                  { border: border, text: '%', fontSize: 10, color: '#6C6C6C' },
                 ],
                 [
-                  { border: border, text: 'TOTAL TAX', fontSize: 10, style: 'itemsFooterSubTitle', color: '#6C6C6C' },
-                  { border: border, text: currencySymbol + vatTotal.toFixed(2), fontSize: 10, style: 'itemsFooterSubValue', alignment: 'right', color: '#6C6C6C' },
-                  { border: border, text: currency.device, fontSize: 10, style: 'itemsFooterSubValue', color: '#6C6C6C' }
+                  { border: border, text: 'TOTAL TAX', fontSize: 10, color: '#6C6C6C' },
+                  { border: border, text: currencySymbol + vatTotal.toFixed(2), fontSize: 10, alignment: 'right', color: '#6C6C6C' },
+                  { border: border, text: currency.device, fontSize: 10, color: '#6C6C6C' }
                 ],
                 [
-                  { border: [false, false, false, false], text: 'BALANCE DUE', fontSize: 10, style: 'itemsFooterTotalTitle', bold: true, color: '#EA663B' },
-                  { border: [false, false, false, false], text: currencySymbol + invoiceTotal.toFixed(2), fontSize: 10, style: 'itemsFooterTotalValue', bold: true, alignment: 'right', color: '#ffffff', fillColor: '#EA663B' },
-                  { border: [false, false, false, false], text: currency.device, fontSize: 10, style: 'itemsFooterSubValue', color: '#ffffff', fillColor: '#EA663B' }
+                  { border: [false, false, false, false], text: 'BALANCE DUE', fontSize: 10, bold: true, color: '#EA663B' },
+                  { border: [false, false, false, false], text: currencySymbol + invoiceTotal.toFixed(2), fontSize: 10, bold: true, alignment: 'right', color: '#ffffff', fillColor: '#EA663B' },
+                  { border: [false, false, false, false], text: currency.device, fontSize: 10, color: '#ffffff', fillColor: '#EA663B' }
                 ],
               ]
             },
@@ -503,7 +397,7 @@ amountTable = function (serviceTotal, vatTotal, vatValue, invoiceTotal, discount
   };
 };
 
-bottom = function (currency, country, vatok, sasu, rcs) {
+bottom = function (currency, country, vatok) {
   return {
     border: [false, false, false, false],
     table: {
@@ -514,7 +408,7 @@ bottom = function (currency, country, vatok, sasu, rcs) {
           text: ' Wire transfer ', bold: true, fontSize: 10, color: '#303030'
         },
         drawLine(0, 60, 0, 0, 1, 'line', '#EA663B'),
-        wireTransfer(currency, sasu, rcs, country, vatok),
+        wireTransfer(currency, country, vatok),
         contactInformation(),
         drawLine(5, 580, 0, 0, 1, 'line', '#EA663B')
       ]]]
@@ -592,7 +486,7 @@ generalPaymentTerms = function () {
                     {
                       widths: ['100%'],
                       table: {
-                        body: condition(['The amount must be paid in full without deducting any bank charges',
+                        body: concatGeneralTerms(['The amount must be paid in full without deducting any bank charges',
                           'No discount for early payment',
                           'Late payment fee equal to 1.5 times the French legal rate of interest, form the due date until the date of payment',
                           'Surchage of EUR 40 for collection costs in case of late payment',
@@ -624,3 +518,61 @@ function vatType(country, vatok) {
     return "Outside the territorial scope – Article 44 of Directive 2006/112/EC";
   }
 }
+
+
+concatGeneralTerms = function (terms) {
+  let termsList = [];
+  termsList.push([{ text: 'General payment terms', bold: true, fontSize: 9, color: '#303030', fillColor: '#F6F6F6' }]);
+  terms.forEach(term => {
+    termsList.push([{ text: '- ' + term, fontSize: 9, color: '#6C6C6C', fillColor: '#F6F6F6' }]);
+  });
+  return termsList;
+};
+
+
+wireTransfer = function (currency, country, vatok) {
+  const beneficiaryName = 'Quanthouse';
+  const beneficiaryAddress = '86 boulevard Haussmann - 75008 PARIS - FRANCE';
+  const vat = 'FR00449703248';
+  let bic = 'BIC Code :';
+  let bank = 'Receiving Bank Name :';
+  let iban = 'IBAN :';
+  let sasu = 'SASU au capital de 14.108.818 €';
+  let rcs = 'RCS Paris 449703248';
+
+  if (currency.id == 'usd' || currency.id == 'gbp') {
+    bic = '';
+    bank = '';
+    iban = '';
+  }
+  return {
+    widths: ['30%', '10%', '25%', '10%', '25%'],
+    table: {
+      body: [[
+        [
+          [{ text: [{ text: 'Beneficiary Name : ', fontSize: 9, bold: true, color: '#303030' }, { text: beneficiaryName, fontSize: 9, color: '#6C6C6C' }] }],
+          [{ text: [{ text: 'Beneficiary Address : ', fontSize: 9, bold: true, color: '#303030' }, { text: beneficiaryAddress, fontSize: 9, color: '#6C6C6C' }] }],
+          [{ text: [{ text: 'VAT : ', fontSize: 9, bold: true, color: '#303030' }, { text: vat, fontSize: 9, color: '#6C6C6C' }] }]
+        ],
+        [
+          { text: '' }
+        ],
+        [
+          [{ text: sasu, fontSize: 9, color: '#6C6C6C' }],
+          [{ text: rcs, fontSize: 9, color: '#6C6C6C' }],
+          [{ text: vatType(country, vatok), fontSize: 9, color: '#6C6C6C' }]
+        ],
+        [
+          { text: '' }
+        ],
+        [
+          [{ text: [{ text: bic, fontSize: 9, bold: true, color: '#303030' }, { text: currency.bic, fontSize: 9, color: '#6C6C6C' }] }],
+          [{ text: [{ text: bank, fontSize: 9, bold: true, color: '#303030' }, { text: currency.rib.domiciliation, fontSize: 9, color: '#6C6C6C' }] }],
+          [{ text: [{ text: iban, fontSize: 9, bold: true, color: '#303030' }, { text: currency.iban.ib1 + ' ' + currency.iban.ib2 + ' ' + currency.iban.ib3 + ' ' + currency.iban.ib4 + ' ' + currency.iban.ib5 + ' ' + currency.iban.ib6 + ' ' + currency.iban.ib7, fontSize: 9, color: '#6C6C6C' }] }]
+        ]
+      ]
+      ]
+    },
+    layout: { defaultBorder: false }
+  };
+};
